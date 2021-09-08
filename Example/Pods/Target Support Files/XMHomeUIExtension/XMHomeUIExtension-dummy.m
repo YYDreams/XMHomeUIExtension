c0b626a3e4dedbf2cd6c47de2694d4ea42368f54
flowerflower <799431668@qq.com>
2021-09-08 14:14:24 +0800

    保存

---
+++ Example/Pods/Target Support Files/XMHomeUIExtension/XMHomeUIExtension-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XMHomeUIExtension : NSObject
+@end
+@implementation PodsDummy_XMHomeUIExtension
+@end
---
+++ XMHomeUIExtension/Classes/XMHomeUI_Extension.swift
@@ -0,0 +1,54 @@
+//
+//  XMHomeUI_Extension.swift
+//  XMHomeUIExtension
+//
+//  Created by flowerflower on 2021/9/2.
+//
+
+import Foundation
+import XMMediator
+
+
+/// Public
+public extension XEMediator {
+    
+
+    @objc func XMHomeUI_homeViewController() -> UIViewController? {
+        if let controller = self.performTarget_XMHomeUI(action: "homeViewController", param: nil) {
+            return controller
+        }
+        return nil
+    }
+
+    
+}
+
+
+/// Private
+extension XEMediator {
+    
+    
+    /// 账号模块通用调用方法
+    ///
+    /// - Parameters:
+    ///   - action: 方法
+    ///   - param: 参数
+    ///   - shouldCacheTarget: 是否缓存 Target
+    func performTarget_XMHomeUI(action: String,
+                                   param: Dictionary<AnyHashable, Any>? = nil,
+                                   shouldCacheTarget: Bool? = false) -> UIViewController? {
+        let targetName = "XMHomeUI"
+        var paramDic: Dictionary<AnyHashable, Any> = param ?? [:]
+        paramDic[kXEMediatorParamsKeySwiftTargetModuleName] = targetName
+        if let controller = self.performTarget(targetName,
+                                               action: action,
+                                               params: paramDic,
+                                               shouldCacheTarget: false) as? UIViewController {
+            return controller
+        }
+        return nil
+    }
+    
+}
+
+
